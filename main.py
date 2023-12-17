db62b4fcdc84fcf520cc5e26de8b563dcba3ebc1
Gerardgfc <gerardo.carrizo123.gc@gmail.com>
2023-12-17 03:50:45 -0300

    creado el modelo ITEM con pydantic

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from typing import Union
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 # Creacion de Aplicacion FastAPI
 
 app = FastAPI()
+
+class Item(BaseModel): 
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None 
 
 @app.get('/')
 def read_root():
@@ -17,3 +23,11 @@ def read_root():
 def read_item(item_id: int, q: Union[str, None] = None):
     return {'item_id': item_id, 'q': q}
 
+@app.get('/calculadora')
+
+def calcular(num1: int, num2: int):
+    return {'suma': num1 + num2}
+
+#solicitud de tipo PUT
+#PUT = Actualizar
+#POST = Crear
