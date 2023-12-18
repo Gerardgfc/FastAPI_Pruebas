96742ce0c028a620b39e05fe691e0032bac4eebb
Gerardgfc <gerardo.carrizo123.gc@gmail.com>
2023-12-17 22:03:35 -0300

    parametros con tipos de datos

--- S04/02_Mejora_Autocompletado.py
+++ S04/02_Mejora_Autocompletado.py
@@ -1,6 +1,7 @@
-def get_full_name(first_name, last_name):
+def get_full_name(first_name: str, last_name:str):
     full_name = first_name.title() + " " + last_name.title()
     return full_name
 
 print(get_full_name("john", "doe")) #john doe
 #print(get_full_name("john", 0))   #AttributeError: 'int' object has no attribute 'title'
+print(get_full_name("julia" "ortiz"))
